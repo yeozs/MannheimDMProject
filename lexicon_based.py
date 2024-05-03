0f13f53a450af4f9fbd29f30524a2de06e3f118b
antoniooliveira03 <tzpoliveira@gmail.com>
2024-05-03 14:55:27 +0200

    dataset treated and updates do lexicon based

--- lexicon_based.py
+++ lexicon_based.py
@@ -1,19 +1,27 @@
 from nltk.sentiment import SentimentIntensityAnalyzer
 vader = SentimentIntensityAnalyzer()
+from textblob import TextBlob
 
 # VADER 
 def vader_algorithm(review, compound = True):
 
-    # Get the polarity scores (negative, neutral and positive) for the song
+    # Get the polarity scores (negative, neutral and positive) for the review
     polarity_scores_ = vader.polarity_scores(review)
     
-    # If you want to compound the scores into one final score
+    # compound scores
     if compound:
         polarity = polarity_scores_["compound"]
 
-    # If you want the three scores
     else:
         # The three separated scores
         polarity = polarity_scores_
+
+    return polarity
+
+# TextBlob
+def textblob_sa(song):
+
+    # Get the polarity (compounded polarity scores) for the song
+    polarity = TextBlob(song).sentiment.polarity
 
     return polarity
